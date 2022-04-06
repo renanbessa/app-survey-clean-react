30f5b2acd64196d502419323fbc3a0ce730bd00d
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-04-06 08:54:31 -0300

    fix: fallback version react-dom-router

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Login } from "@/presentation/pages";
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" exact component={Login} />
       </Switch>
     </BrowserRouter>
   );

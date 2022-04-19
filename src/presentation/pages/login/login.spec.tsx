0440b790ea51296cdd33918d6e5d1d75c5e5cf64
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-04-19 09:15:50 -0300

    test: ensure Login navigates to signup correctly

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -11,6 +11,8 @@ import {
 import Login from "./login";
 import { AuthenticationSpy, ValidationStub } from "@/presentation/test/";
 import { InvalidCredentialsError } from "@/domain/errors";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 type SutTypes = {
   sut: RenderResult;
@@ -21,12 +23,16 @@ type SutParams = {
   validationError: string;
 };
 
+const history = createMemoryHistory();
+
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub();
   const authenticationSpy = new AuthenticationSpy();
   validationStub.errorMessage = params?.validationError;
   const sut = render(
-    <Login validation={validationStub} authentication={authenticationSpy} />
+    <Router history={history}>
+      <Login validation={validationStub} authentication={authenticationSpy} />
+    </Router>
   );
   return {
     sut,
@@ -180,5 +186,13 @@ describe("Login Component", () => {
       "accessToken",
       authenticationSpy.account.accessToken
     );
+  });
+
+  test("Should go to signup page", () => {
+    const { sut } = makeSut();
+    const register = sut.getByTestId("signup");
+    fireEvent.click(register);
+    expect(history.length).toBe(2);
+    expect(history.location.pathname).toBe("/signup");
   });
 });

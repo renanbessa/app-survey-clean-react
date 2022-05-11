ef6fa068fe0afd74427f0cf86a30f6cb9e0cdd88
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-05-10 23:49:48 -0300

    test: ensure MinLenght return valid and falsy

---
+++ src/validation/validators/min-lenght/min-lenght-validation.spec.ts
@@ -0,0 +1,19 @@
+import { InvalidFieldError } from "@/validation/errors";
+import { MinLenghtValidation } from "./min-lenght-validation";
+import faker from "faker";
+
+const makeSut = (): MinLenghtValidation =>
+  new MinLenghtValidation(faker.database.column(), 5);
+describe("MinLenghtValidation", () => {
+  test("Should return error if value is invalid ", () => {
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(4));
+    expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test("Should return falsy if value is valid ", () => {
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(5));
+    expect(error).toBeFalsy();
+  });
+});

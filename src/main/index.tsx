84a11f84bd68e649676cfad827d465e81c72dd89
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-05-19 08:57:21 -0300

    feat: add Login composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@/presentation/components";
 import "@/presentation/styles/global.scss";
+import { MakeLogin } from "./factories/pages/login/login-factory";
 
-ReactDOM.render(<Router />, document.getElementById("main"));
+ReactDOM.render(
+  <Router makeLogin={MakeLogin} />,
+  document.getElementById("main")
+);

01dce2671cc90ce9464af27390c69092c6f465e7
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-05-24 09:28:38 -0300

    feat: add env var to API URL

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,60 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const { DefinePlugin } = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main/index.tsx',
+  mode: "development",
+  entry: "./src/main/index.tsx",
   output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js'
+    path: path.join(__dirname, "public/js"),
+    publicPath: "/public/js",
+    filename: "bundle.js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    extensions: [".ts", ".tsx", ".js", "scss"],
     alias: {
-      '@': path.join(__dirname, 'src')
-    }
+      "@": path.join(__dirname, "src"),
+    },
   },
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader',
-        options: {
-          modules: true
-        }
-      }, {
-        loader: 'sass-loader'
-      }]
-    }]
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
   devServer: {
-    contentBase: './public',
+    contentBase: "./public",
     writeToDisk: true,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
+    react: "React",
+    "react-dom": "ReactDOM",
   },
   plugins: [
-    new CleanWebpackPlugin()
-  ]
-}
+    new CleanWebpackPlugin(),
+    new DefinePlugin({
+      "process.env.API_URL": "https://fordevs.herokuapp.com/api",
+    }),
+  ],
+};

7761461c854b97a62de7e4cb0b7352eedae28866
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-06-09 08:53:08 -0300

    test: ensure RemoteAddAccount throw EmailInUseError if HttpPostClient returns 403

--- src/data/usecases/add-account/add-account.spec.ts
+++ src/data/usecases/add-account/add-account.spec.ts
@@ -1,6 +1,8 @@
 import { RemoteAddAccount } from "./add-account";
 import { HttpPostClientSpy } from "@/data/test";
-import { mockAddAccount } from "@/domain/test";
+import { HttpStatusCode } from "@/data/protocols/http";
+import { EmailInUseError } from "@/domain/errors";
+import { mockAddAccountParams } from "@/domain/test";
 import { AddAccountParams } from "@/domain/usecases";
 import { AccountModel } from "@/domain/models";
 import faker from "faker";
@@ -26,14 +28,23 @@ describe("AddAccount", () => {
   test("Should call HttpPostClient with correct URL", async () => {
     const url = faker.internet.url();
     const { sut, httpPostClientSpy } = makeSut(url);
-    await sut.add(mockAddAccount());
+    await sut.add(mockAddAccountParams());
     expect(httpPostClientSpy.url).toBe(url);
   });
 
   test("Should call HttpPostClient with correct body", async () => {
     const { sut, httpPostClientSpy } = makeSut();
-    const authenticationParams = mockAddAccount();
+    const authenticationParams = mockAddAccountParams();
     await sut.add(authenticationParams);
     expect(httpPostClientSpy.body).toEqual(authenticationParams);
+  });
+
+  test("Should throw EmailInUseError if HttpPostClient returns 403", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden,
+    };
+    const promise = sut.add(mockAddAccountParams());
+    await expect(promise).rejects.toThrow(new EmailInUseError());
   });
 });
--- src/domain/test/mock-add-account.ts
+++ src/domain/test/mock-add-account.ts
@@ -1,7 +1,7 @@
 import { AddAccountParams } from "@/domain/usecases";
 import faker from "faker";
 
-export const mockAddAccount = (): AddAccountParams => {
+export const mockAddAccountParams = (): AddAccountParams => {
   const password = faker.internet.password();
   return {
     name: faker.name.findName(),

cf35640204601acbfe111d7498e555b34a1ad9c7
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-06-10 11:59:45 -0300

    feat: add signup view

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,3 +1,4 @@
+import { Signup } from "@/presentation/pages";
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
@@ -10,6 +11,7 @@ const Router: React.FC<Props> = ({ makeLogin }: Props) => {
     <BrowserRouter>
       <Switch>
         <Route path="/login" exact component={makeLogin} />
+        <Route path="/signup" exact component={Signup} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/presentation/pages/signup/signup.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Styles from "./signup-styles.scss";
+import {
+  Footer,
+  Input,
+  LoginHeader,
+  FormStatus,
+} from "@/presentation/components";
+import Context from "@/presentation/contexts/form/form-context";
+import { Link } from "react-router-dom";
+
+const Signup: React.FC = () => {
+  return (
+    <div className={Styles.signup}>
+      <LoginHeader />
+      <Context.Provider value={{ state: {} }}>
+        <form className={Styles.form}>
+          <h2>Criar Conta</h2>
+          <Input type="text" name="name" placeholder="Digite seu nome" />
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <Input
+            type="password"
+            name="passwordConfirm"
+            placeholder="Confirme sua senha"
+          />
+          <button className={Styles.submit} type="submit">
+            Criar Conta
+          </button>
+          <Link to="/login" className={Styles.link}>
+            Login
+          </Link>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  );
+};
+
+export default Signup;
